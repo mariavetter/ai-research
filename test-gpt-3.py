514f1704ace4883009618eeb70ad0f59ffa1f8ab
Tim Habeck <th135@hdm-stuttgart.com>
2023-01-26 11:53:14 +0100

    daten mit gpt 3 generiert

--- test-gpt-3.py
+++ test-gpt-3.py
@@ -1,4 +1,4 @@
-Api_key = "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"
+Api_key = "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"
 
 
 import openai
@@ -6,19 +6,19 @@ import time
 
 openai.api_key = Api_key
 
-amount = 100
+amount = 267
 
-prompt = "Write the text of another spam Mail without a subject for testing a spam filter."\
+prompt = "Write only the text of a generic genuine with no placeholders mail."\
     "Response:"
 
 
-with open("spam_gen.txt", "a+") as f:
-    for i in range(amount):
-        response = openai.Completion.create(engine="text-davinci-003", prompt=prompt, max_tokens=50)
-        text = response["choices"][0]["text"]
-        text = text.replace("\n", "").replace(r"\r\n", "")
-        text = text + "\n"
-        out = text.replace('\n', '')
-        print(f"{i}: {out}")
+for i in range(amount):
+    response = openai.Completion.create(engine="text-davinci-003", prompt=prompt, max_tokens=70)
+    text = response["choices"][0]["text"]
+    text = text.replace("\n", "").replace(r"\r\n", "")
+    text = text + "\n"
+    out = text.replace('\n', '')
+    print(f"{i}: {out}")
+    with open("normal_gen.txt", "a+") as f:
         f.writelines(text)
-        time.sleep(1.1)
+    time.sleep(1.3)
